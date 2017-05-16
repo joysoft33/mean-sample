37cf11f393c2a68385962243ae1e6ecead48bd82
Eric LEON <ericp.leon@gmail.com>
2017-05-16 10:46:09 +0200

    Webpack config file cleaned

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CleanObsoleteChunks = require('webpack-clean-obsolete-chunks');
 
-const NODE_MODULES = path.join(__dirname, './node_modules');
 const PRODUCTION = process.env.NODE_ENV === 'production';
 
 /**
@@ -83,24 +82,13 @@ const clientConfig = {
         'img-loader'
       ]
     }, {
-      test: /\.css$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader',
-          options: {
-            minimize: true
-          }
-        }]
-      })
-    }, {
-      test: /\.scss$/,
+      test: /\.s?css$/,
       use: ExtractTextPlugin.extract({
         fallback: 'style-loader',
         use: [{
             loader: 'css-loader',
             options: {
-              minimize: true
+              minimize: PRODUCTION
             }
           },
           {
@@ -137,53 +125,6 @@ const clientConfig = {
   ].filter(e => e),
   devtool: PRODUCTION ? 'source-map' : 'cheap-module-eval-source-map',
 };
-
-// Run through deps and extract the first part of the path, 
-// as that is what you use to require the actual node modules 
-// in your code. Then use the complete path to point to the correct
-// file and make sure webpack does not try to parse it
-// if (PRODUCTION) {
-
-//   var externals = [];
-//   var alias = {};
-
-//   [
-//     {
-//       name: 'angular',
-//       file: 'angular.min.js'
-//     },
-//     {
-//       name: 'angular-animate',
-//       file: 'angular-animate.min.js'
-//     },
-//     {
-//       name: 'angular-aria',
-//       file: 'angular-aria.min.js'
-//     },
-//     {
-//       name: 'angular-material',
-//       file: 'angular-material.min.js'
-//     },
-//     {
-//       name: 'angular-css',
-//       file: 'angular-css.min.js'
-//     },
-//     {
-//       name: 'angular-ui-router',
-//       file: 'release/angular-ui-router.min.js'
-//     }
-//   ]
-//   .forEach(function (dep) {
-//     var depPath = path.resolve(NODE_MODULES, path.join(dep.name, dep.file));
-//     alias[dep.name] = depPath;
-//     externals.push(dep.name);
-//   });
-
-//   clientConfig.externals = externals;
-//   clientConfig.resolve = {
-//     alias: alias
-//   };
-// }
 
 // Notice that both configurations are exported
 module.exports = [serverConfig, clientConfig];
