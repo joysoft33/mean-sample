104aab9331e1a0e1e7cd0d74b1c0adc930dce210
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2017-06-20 14:10:01 +0200

    last elements before push heroku

--- server/config/env.js
+++ server/config/env.js
@@ -9,12 +9,12 @@ let config = {
     facebookAuth: {
       clientID: process.env.FACEBOOK_ClientId,
       clientSecret: process.env.FACEBOOK_ClientSecret,
-      callbackURL: '/api/auth/facebook/callback'
+      callbackURL: '/auth/facebook/callback'
     }
   },
 
   production: {
-    db: process.env.MONGODB_URI || 'mongodb://localhost:27017/mean-sample',
+    db: process.env.MONGODB_URI || 'mongodb://heroku_k38xhh52:3fpqr8knth6fs4q0pr5r605qco@ds131512.mlab.com:31512/heroku_k38xhh52',
     cookieToken: 'mean-token',
     jwtSecret: 'cJbdB3t$',
     facebookAuth: {
